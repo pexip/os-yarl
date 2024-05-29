06c8996f718f640906c084ad9557d03da45a2d2b
Huw Jones <huw@pexip.com>
2024-05-29 11:34:52 +0100

    Import Upstream version 1.8.2

--- tests/test_url_query.py
+++ tests/test_url_query.py
@@ -2,7 +2,6 @@ from urllib.parse import urlencode
 
 from multidict import MultiDict, MultiDictProxy
 
-
 from yarl import URL
 
 # query
@@ -60,14 +59,3 @@ def test_ampersand_as_value():
     u = URL("http://127.0.0.1/?a=1%26b=2")
     assert len(u.query) == 1
     assert u.query["a"] == "1&b=2"
-
-
-def test_semicolon_as_separator():
-    u = URL("http://127.0.0.1/?a=1;b=2")
-    assert len(u.query) == 2
-
-
-def test_semicolon_as_value():
-    u = URL("http://127.0.0.1/?a=1%3Bb=2")
-    assert len(u.query) == 1
-    assert u.query["a"] == "1;b=2"
